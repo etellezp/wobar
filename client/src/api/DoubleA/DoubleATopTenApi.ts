675a012f42a03caac7caf20cdee68796ff5b2ad3
Enrique Tellez <etellezp@gmail.com>
2024-03-26 13:49:43 -0400

    setup prod url in doublA api

--- client/src/api/DoubleA/DoubleATopTenApi.ts
+++ client/src/api/DoubleA/DoubleATopTenApi.ts
@@ -2,14 +2,14 @@ import axios from 'axios'
 
 export const getDoubleATopTenBatters = async (year: string) => {
 	const response = await axios.get(
-		`http://localhost:8000/doubleARatings/${year}/top10/Batter`
+		`https://wobar-api.onrender.com/doubleARatings/${year}/top10/Batter`
 	)
 	return response ? response.data : []
 }
 
 export const getDoubleATopTenPitchers = async (year: string) => {
 	const response = await axios.get(
-		`http://localhost:8000/doubleARatings/${year}/top10/Pitcher`
+		`https://wobar-api.onrender.com/doubleARatings/${year}/top10/Pitcher`
 	)
 	return response ? response.data : []
 }
