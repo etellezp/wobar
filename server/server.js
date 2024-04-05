e64b369b00829ea7d5fca62b7fc3daad935a0c44
etellezp <etellezp@gmail.com>
2024-04-04 22:34:37 -0400

    added own url

--- server/server.js
+++ server/server.js
@@ -11,8 +11,9 @@ const app = express()
 const port = process.env.PORT || 8000
 
 const corsOptions = {
-	origin: 'https://wobar.onrender.com'
+	// origin: 'https://wobar.onrender.com'
 	// origin: 'http://localhost:5173'
+	origin: 'https://worldbaseballrating.com'
 }
 
 // Middlewares
